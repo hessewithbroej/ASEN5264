3e4e0fea38f427bead211ef130e58bf7a3b77438
Joe Hesse-Withbroe <hessewithbroej@hotmail.com>
2024-04-15 17:03:18 -0600

    working gold HMM solver using Baum-welch

---
+++ main.jl
@@ -0,0 +1,41 @@
+import HiddenMarkovModels as hmms
+import CSV
+import DataFrames as DF
+import Random
+import Distributions as dists
+import Plots as plt
+import Dates
+
+Random.seed!(12321)
+
+#load data, add a differential column
+data = DF.DataFrame(CSV.File("C:/Users/hesse/Desktop/Code/ASEN5264/gold_price_usd.csv"))
+data.gold_price_usd_diff = [0; diff(data.gold_price_usd)]
+
+#data restrict to after to jan 1 2008
+after_date(date::Dates.Date) = date > Dates.Date(2008,1,1)
+data_restricted = filter(:datetime => after_date, data)
+
+#get data in differential form, supposedly easier to model
+# deleteat!(data_restricted, [1])
+# @show data_restricted
+
+#visualize raw data
+plt_raw = display(plt.plot(data_restricted.datetime, data_restricted.gold_price_usd))
+plt_raw_diff = display(plt.plot(data_restricted.datetime, data_restricted.gold_price_usd_diff))
+
+
+
+# #baum-welch attempt
+init_guess = [0.9, 0.05, 0.05]
+trans_guess = [0.8 0.15 0.05; 0.10 0.80 0.10; 0.05 0.1 0.85]
+dists_guess = [dists.Normal(1,5), dists.Normal(1,10), dists.Normal(1,20)]
+
+hmm_guess = hmms.HMM(init_guess, trans_guess, dists_guess)
+hmm_est, llh_evolution = hmms.baum_welch(hmm_guess,data_restricted.gold_price_usd_diff)
+
+println("done")
+#plot hmm characterizations
+
+
+
