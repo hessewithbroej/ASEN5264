f85222e058e06acbb3402d84141c74629be62f27
pravesh-ranav <praveshranav@gmail.com>
2024-05-07 15:34:13 -0600

    Add files via upload
    FullmodelHMM

--- main.jl
+++ main.jl
@@ -8,19 +8,26 @@ include("HelperFunctions.jl")
 import .HelperFunctions as hf
 import Plots as plt
 
-
+gt_trust = DF.DataFrame(XLSX.readdata("FeaturesForModelsAFP30.xlsx", "Trust", "A2:A193"), :auto)
+gt_trust = Float64[x for x in gt_trust.x1]
+truth_state = [num > 0.5 ? 2 : 1 for num in gt_trust]
 #trust thresholds for new states. In this example, there are two states, one where 0 <= trust < 0.5, and one where 0.5 <= trust.
 states = [0.0, 0.5]
 
 #these should be tuples of sheet name and column number for the feature you want to use
-# using version 4 of every BIOPAC feature 
-ind = collect(11:9:153)
-features = [("Features",2)]
-for i in ind
-    push!(features,("Features",i))
-end
+# features = [("ECG_SDNN",5),("EDA_NumSCRs",3),("EDA_PhasicMax",5),("RSP_MV",5),("RSP_RR",5)]
+# features = [("ECG_SDNN",5), ("EDA_NumSCRs",3), ("EDA_PhasicMax",5), ("RSP_MV",5), ("RSP_MV",7), ("RSP_RR",5)]
+# features = [("Features",3),("Features",22)]   #71fNIRS_Vers4 265
+# features = [("fNIRS_Vers4",71),("fNIRS_Vers4",265)]
+features_neuro = [("fNIRS_Vers4",18),("fNIRS_Vers4",20),("fNIRS_Vers4",44),("fNIRS_Vers4",45),("fNIRS_Vers4",49),("fNIRS_Vers4",52),("fNIRS_Vers4",77),("fNIRS_Vers4",96),("fNIRS_Vers4",102),("fNIRS_Vers4",107),("fNIRS_Vers4",121),("fNIRS_Vers4",123),("fNIRS_Vers4",124),("fNIRS_Vers4",128),("fNIRS_Vers4",160),("fNIRS_Vers4",225),("fNIRS_Vers4",240),("fNIRS_Vers4",277),("fNIRS_Vers4",290),("fNIRS_Vers4",322),("fNIRS_Vers4",338),("fNIRS_Vers4",345),("fNIRS_Vers4",348),("fNIRS_Vers4",357)]
+features_physio = [("Features",2),("Features",11),("Features",20),("Features",29), ("Features",38),("Features",47),("Features",56),("Features",65),("Features",74),("Features",83),("Features",92),("Features",101),("Features",110),("Features",119),("Features",128),("Features",137),("Features",146)]
 
-data_files = ["C:/Users/SARAH/Desktop/DMU/Project/ASEN5264/FeaturesForModelsAFP30.xlsx"]
+features = append!(features_neuro,features_physio)
+# features = features_physio
+data_files = ["FeaturesForModelsAFP30.xlsx"]
+
+# data_files = ["C:/Users/hesse/Desktop/Code/ASEN5264/AFP30/AFP30_S1_Features.xlsx","C:/Users/hesse/Desktop/Code/ASEN5264/AFP30/AFP30_S2_Features.xlsx","C:/Users/hesse/Desktop/Code/ASEN5264/AFP30/AFP30_S3_Features.xlsx","C:/Users/hesse/Desktop/Code/ASEN5264/AFP30/AFP30_S4_Features.xlsx","C:/Users/hesse/Desktop/Code/ASEN5264/AFP31/AFP31_S1_Features.xlsx","C:/Users/hesse/Desktop/Code/ASEN5264/AFP31/AFP31_S2_Features.xlsx","C:/Users/hesse/Desktop/Code/ASEN5264/AFP31/AFP31_S3_Features.xlsx","C:/Users/hesse/Desktop/Code/ASEN5264/AFP31/AFP31_S4_Features.xlsx"]
+
 
 data = hf.merge_data(data_files,features)
 hf.plot_observation_distributions(states,features,data)
@@ -31,6 +38,7 @@ init_guess = [0.5,0.5]
 hmm_guess = hmms.HMM(init_guess, trans_guess, dists_guess)
 
 @show obs_seq,seq_ends = hf.thread_observations(data_files,features)
+
 
 @show hmm_est, llh_evolution = hmms.baum_welch(hmm_guess,obs_seq;seq_ends)
 
@@ -49,15 +57,8 @@ for i=1:length(seq_ends)
 end
 
 studies = unique(data.SessionID)
-plt_viterbi = plt.scatter([],[],xlabel="Observation #",ylabel="Predicted Trust Response")
-pop!(plt_viterbi.series_list)
-for j=1:length(studies)
-    data_subset = data[data.SessionID.==studies[j],:]
-    for i=1:length(states)
-        inds = findall(x -> x==i, best_seqs[j])
-        x = tmp[inds].+(48*(j-1))
-        y = data_subset.Trust[inds]
-        plt.scatter!(x,y,color=colors[i],markershape=shapes[j],legend=false)
-    end
-end
-display(plt_viterbi)
+plt_obj = plt.scatter( 1:length(truth_state), gt_trust, label=false,color = [value1 == value2 ? "green" : "red" for (value1, value2) in zip(truth_state, best_state_seq) ], xlabel = "Epoch", ylabel = "Ground Truth", title = "Viterbi Classification of Binary Trust Responses", grid = true,ylim=(0,1))
+plt.scatter!([],[],color="green",label="Correctly Predicted Trust");
+plt.scatter!([],[],color="red",label="Incorrectly Predicted Trust",legendfontsize=10);
+display(plt_obj)
+
